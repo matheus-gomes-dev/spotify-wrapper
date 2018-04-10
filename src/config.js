63c385f1f5466d7bc817f39ae0d1254b2b36d94e
matheus-lima92 <lima.eca@gmail.com>
2018-04-09 23:51:00 -0300

    included spotify token

--- src/config.js
+++ src/config.js
@@ -1,2 +1,9 @@
-const API_URL = 'https://api.spotify.com/v1';
-export default API_URL;
+const TOKEN_API = 'BQAX71qcpQTH_fvixKJNhGHIW2sct2dw1u5iErD77AYhijCYsQNOds2b0WAbnF_djcPDk_7Z_PijhSef0JTFYhAbwSWAABEzmDLpcLHYxFkTfOkNM3Qw2bUsAA9hy36YSqMG2pugpEpWQZqNW-z0';
+
+export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+	headers: {
+		Authorization: `'Bearer ${TOKEN_API}'`
+	}
+}
